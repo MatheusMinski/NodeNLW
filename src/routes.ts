9ff20d38616ed9550812fa567b32003624af5e47
Matheus Minski dos Santos <minskisantos@hotmail.com>
2021-04-22 17:42:43 -0300

    Aula 2

--- src/routes.ts
+++ src/routes.ts
@@ -1,24 +1,10 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "./repositories/SettingsRepository";
-
+import { SettingsController } from "./controllers/SettingsController";
 
 const routes = Router();
 
-routes.post("/settings", async (request, response) => {
-    const { chat, username } = request.body;
-    // return response.json("aloha");
-    const setttingsRepository = getCustomRepository(SettingsRepository);
 
-
-    const settings = setttingsRepository.create({
-        chat,
-        username
-    });
-
-    await setttingsRepository.save(settings);
-
-    return response.json(settings);
-})
+const settingsController = new SettingsController();
+routes.post("/settings", settingsController.create)
 
 export { routes }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 import express from "express";
 import "./database";
 import { routes } from "./routes";
 
-const app = express();
 
-app.use(express.json());
+(async () => {
+    await createConnection()
+        .then(connection => {
+            console.log('conexão com o db ok');
+            const app = express();
 
-app.use(routes);
+            app.use(express.json());
 
-app.listen(8888, () => console.log("Server rodando na 8888"));
+            app.use(routes);
+
+            app.listen(8888, () => console.log("Server rodando na 8888"));
+        }).catch(error => console.log(error));
+})()
+
+
